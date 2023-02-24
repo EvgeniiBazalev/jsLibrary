dd59d143687db493eb4f4ce13e3a60820d8b3d81
Evgenii Bazalev <bazalevj@gmail.com>
2023-02-24 15:21:34 +0500

    *js

--- Object/script2.js
+++ Object/script2.js
@@ -1,6 +1,6 @@
 'use strict';
+'use strict';
 
-<<<<<<< HEAD
 let user = {
 
     name: 'Vasia',
@@ -9,12 +9,3 @@ let user = {
     }
 
 };
-
-console.log(user.sername);
-=======
-new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        reject(new Error("Whoops!"));
-    }, 10000);
-}).catch(error => console.log(error));
->>>>>>> cb3de0e56f1d814c603ba6c8bd5b11f874a8e1bc
