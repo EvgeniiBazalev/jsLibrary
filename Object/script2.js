c52303899782d9b21817e0d2dc7f042d7426f9b1
Evgenii <bazalevj@gmail.com>
2023-02-27 08:31:01 +0500

    js 27.02.2023

--- Object/script2.js
+++ Object/script2.js
@@ -1,7 +1,15 @@
 'use strict';
 
-new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        reject(new Error("Whoops!"));
-    }, 10000);
-}).catch(error => console.log(error));
+// Перепишите один из примеров раздела Цепочка промисов, используя async/await
+// вместо .then/catch :
+
+async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+        return response.json();
+    } else {
+        throw new Error(response.status);
+    }
+}
+loadJson('no-such-user.json') // (3)
+    .catch(alert); // Error: 404
